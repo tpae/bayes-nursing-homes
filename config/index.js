f3b74e599f80f7d1b133e7f2d7d653db988cb19a
Terence Pae <terencepae@gmail.com>
2014-11-16 10:06:53 -0800

    updated data list items

---
+++ config/index.js
@@ -0,0 +1,17 @@
+
+module.exports.variables = {
+  cost_self_pay: { description: 'Self pay charges', type: 'heatmap' },
+  num_beds: { description: 'Number of beds', type: 'heatmap' },
+  cost_medicare: { description: 'Medicare charges', type: 'heatmap' },
+  cost_medical: { description: 'Medi-Cal charges', type: 'heatmap' },
+  net_income_margin: { description: 'Net income margin', type: 'heatmap' },
+  occupency_rate: { description: 'Occupency rate', type: 'heatmap' },
+  state_penalty: { description: 'State penalty', type: 'heatmap' },
+  overall_rating: { description: 'CalQualityCare Overall rating', type: 'heatmap' },
+  complaints_deficiencies: { description: 'Compalints and deficiencies', type: 'heatmap' },
+  quality: { description: 'Quality of care', type: 'heatmap' },
+  staff_wages: { description: 'Staff wages', type: 'heatmap' },
+  staff_treatment: { description: 'Staff treatment', type: 'heatmap' },
+  fed_penalty: { description: 'Federal penalty', type: 'heatmap' },
+  age: { description: 'Resident age', type: 'heatmap' }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 
 var express = require('express');
 var router = express.Router();
+var labels = require('../config').variables;
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { labels: labels });
 });
 
 module.exports = router;
