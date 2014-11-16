967322497aa2f4edc8eddbfab008afac4f5673bd
Terence Pae <terencepae@gmail.com>
2014-11-16 10:08:48 -0800

    updated copy

--- config/index.js
+++ config/index.js
@@ -9,9 +9,9 @@ module.exports.variables = {
   state_penalty: { description: 'State penalty', type: 'heatmap' },
   overall_rating: { description: 'CalQualityCare Overall rating', type: 'heatmap' },
   complaints_deficiencies: { description: 'Compalints and deficiencies', type: 'heatmap' },
-  quality: { description: 'Quality of care', type: 'heatmap' },
+  quality: { description: 'CalQualityCare Quality of care rating', type: 'heatmap' },
   staff_wages: { description: 'Staff wages', type: 'heatmap' },
-  staff_treatment: { description: 'Staff treatment', type: 'heatmap' },
+  staff_treatment: { description: 'Staff rating', type: 'heatmap' },
   fed_penalty: { description: 'Federal penalty', type: 'heatmap' },
   age: { description: 'Resident age', type: 'heatmap' }
 };
