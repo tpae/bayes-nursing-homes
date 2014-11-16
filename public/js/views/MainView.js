72c8638ddb0a4d64df2b7896cbaad45971f288af
Terence Pae <terencepae@gmail.com>
2014-11-16 10:16:38 -0800

    updated mainview with default date filter

--- public/js/views/MainView.js
+++ public/js/views/MainView.js
@@ -7,7 +7,6 @@ define([
   var MainView = Backbone.View.extend({
     events: {
       'click .tabs-toggle': 'toggleFooter',
-      'click .mapToggle': 'toggleMap',
       'keydown #search-form': 'onlyNumbers',
       'submit #search-form': 'search'
     },
@@ -17,10 +16,12 @@ define([
       this.query = props.query;
       this.map = props.map;
 
-      this.$('.mapToggle.selected').each(function(i, element) {
+      $('.mapToggle.selected').each(function(i, element) {
         var element = $(element);
         self.query.push(element.data('key'));
       });
+
+      $('.mapToggle').on('click', this.toggleMap.bind(this));
 
       this.pullCongress();
     },
@@ -86,8 +87,10 @@ define([
     toggleFooter: function(e) {
       var target = $(e.currentTarget);
       if (!target.hasClass('oldpeople')) {
+        this.query.remove($('.dateFilter.selected').data('key'));
         $('#footer').slideUp(75);
       } else {
+        this.query.push($('.dateFilter.selected').data('key'));
         $('#footer').slideDown(75);
       }
     }
